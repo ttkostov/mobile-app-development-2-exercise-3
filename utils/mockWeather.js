618f3ef476052bd58a3134b4ef3662300c1b1bed
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-25 16:44:04 +0300

    update mock weather to use weather entry objects

--- utils/mockWeather.js
+++ utils/mockWeather.js
@@ -1,52 +1,27 @@
 // ChatGPT was used for generating the mock weather
-export const MOCK_WEATHER = [
-    {
-        id: 1,
-        city: 'Helsinki, FI',
-        temperatureC: 12.3,
-        windKmh: 18,
-        code: 1
-    },
-    {
-        id: 2,
-        city: 'Espoo, FI',
-        temperatureC: 12.0,
-        windKmh: 16,
-        code: 1
-    },
-    {
-        id: 3,
-        city: 'Tampere, FI',
-        temperatureC: 11.0,
-        windKmh: 14,
-        code: 3
-    },
-    {
-        id: 4,
-        city: 'Vantaa, FI',
-        temperatureC: 12.1,
-        windKmh: 17,
-        code: 2
-    },
-    {
-        id: 5,
-        city: 'Oulu, FI',
-        temperatureC: 9.5,
-        windKmh: 20,
-        code: 3
-    },
-    {
-        id: 6,
-        city: 'Turku, FI',
-        temperatureC: 12.5,
-        windKmh: 15,
-        code: 2
-    },
-    {
-        id: 7,
-        city: 'Jyväskylä, FI',
-        temperatureC: 10.0,
-        windKmh: 13,
-        code: 1
+export class WeatherEntry {
+    /**
+     * @param {number} id
+     * @param {string} city
+     * @param {number} temperatureC
+     * @param {number} windKmh
+     * @param {1|2|3|4|5|6} code
+     */
+    constructor(id, city, temperatureC, windKmh, code) {
+        this.id = id;
+        this.city = city;
+        this.temperatureC = temperatureC;
+        this.windKmh = windKmh;
+        this.code = code;
     }
+}
+
+export const MOCK_WEATHER = [
+    new WeatherEntry(1, 'Helsinki, FI', 12.3, 18, 1),
+    new WeatherEntry(2, 'Espoo, FI', 12.0, 16, 1),
+    new WeatherEntry(3, 'Tampere, FI', 11.0, 14, 3),
+    new WeatherEntry(4, 'Vantaa, FI', 12.1, 17, 2),
+    new WeatherEntry(5, 'Oulu, FI', 9.5, 20, 3),
+    new WeatherEntry(6, 'Turku, FI', 12.5, 15, 2),
+    new WeatherEntry(7, 'Jyväskylä, FI', 10.0, 13, 1),
 ];
