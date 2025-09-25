dcbaf3c7640d18d3e2d2927fed361efc3e0f971e
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-25 20:12:43 +0300

    update mockWeather to include forecast

--- utils/mockWeather.js
+++ utils/mockWeather.js
@@ -1,27 +1,101 @@
 // ChatGPT was used for generating the mock weather
-export class WeatherEntry {
-    /**
-     * @param {number} id
-     * @param {string} city
-     * @param {number} temperatureC
-     * @param {number} windKmh
-     * @param {1|2|3|4|5|6} code
-     */
-    constructor(id, city, temperatureC, windKmh, code) {
-        this.id = id;
-        this.city = city;
-        this.temperatureC = temperatureC;
-        this.windKmh = windKmh;
-        this.code = code;
-    }
-}
-
-export const MOCK_WEATHER = [
-    new WeatherEntry(1, 'Helsinki', 12.3, 18, 1),
-    new WeatherEntry(2, 'Espoo', 12.0, 16, 1),
-    new WeatherEntry(3, 'Tampere', 11.0, 14, 3),
-    new WeatherEntry(4, 'Vantaa', 12.1, 17, 2),
-    new WeatherEntry(5, 'Oulu', 9.5, 20, 3),
-    new WeatherEntry(6, 'Turku', 12.5, 15, 2),
-    new WeatherEntry(7, 'Jyväskylä', 10.0, 13, 1),
+export const MOCK_WEATHER_WITH_FORECAST = [
+    {
+        id: 1,
+        city: 'Helsinki',
+        current: { temperatureC: 12.3, windKmh: 18, code: 1 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 18, code: 1, precipitationProbability: 40 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 17, code: 2, precipitationProbability: 20 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 11, temperatureMaxC: 15, windKmh: 19, code: 1, precipitationProbability: 50 },
+            { id: 4, day: 'Thursday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 18, code: 3, precipitationProbability: 60 },
+            { id: 5, day: 'Friday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 16, code: 1, precipitationProbability: 30 },
+            { id: 6, day: 'Saturday', temperatureMinC: 10, temperatureMaxC: 15, windKmh: 17, code: 2, precipitationProbability: 20 },
+            { id: 7, day: 'Sunday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 18, code: 1, precipitationProbability: 50 },
+        ]
+    },
+    {
+        id: 2,
+        city: 'Espoo',
+        current: { temperatureC: 12.0, windKmh: 16, code: 1 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 10, temperatureMaxC: 13, windKmh: 16, code: 1, precipitationProbability: 35 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 9, temperatureMaxC: 12, windKmh: 17, code: 2, precipitationProbability: 15 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 16, code: 1, precipitationProbability: 45 },
+            { id: 4, day: 'Thursday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 18, code: 3, precipitationProbability: 55 },
+            { id: 5, day: 'Friday', temperatureMinC: 9, temperatureMaxC: 12, windKmh: 15, code: 1, precipitationProbability: 25 },
+            { id: 6, day: 'Saturday', temperatureMinC: 10, temperatureMaxC: 13, windKmh: 16, code: 2, precipitationProbability: 20 },
+            { id: 7, day: 'Sunday', temperatureMinC: 8, temperatureMaxC: 11, windKmh: 17, code: 1, precipitationProbability: 50 },
+        ]
+    },
+    {
+        id: 3,
+        city: 'Tampere',
+        current: { temperatureC: 11.0, windKmh: 14, code: 3 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 14, code: 3, precipitationProbability: 60 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 15, code: 1, precipitationProbability: 40 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 8, temperatureMaxC: 13, windKmh: 14, code: 3, precipitationProbability: 50 },
+            { id: 4, day: 'Thursday', temperatureMinC: 7, temperatureMaxC: 12, windKmh: 15, code: 1, precipitationProbability: 35 },
+            { id: 5, day: 'Friday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 13, code: 3, precipitationProbability: 55 },
+            { id: 6, day: 'Saturday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 14, code: 2, precipitationProbability: 25 },
+            { id: 7, day: 'Sunday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 15, code: 1, precipitationProbability: 45 },
+        ]
+    },
+    {
+        id: 4,
+        city: 'Vantaa',
+        current: { temperatureC: 12.1, windKmh: 17, code: 2 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 17, code: 1, precipitationProbability: 40 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 16, code: 2, precipitationProbability: 20 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 18, code: 1, precipitationProbability: 50 },
+            { id: 4, day: 'Thursday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 17, code: 3, precipitationProbability: 55 },
+            { id: 5, day: 'Friday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 16, code: 1, precipitationProbability: 30 },
+            { id: 6, day: 'Saturday', temperatureMinC: 10, temperatureMaxC: 14, windKmh: 17, code: 2, precipitationProbability: 20 },
+            { id: 7, day: 'Sunday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 18, code: 1, precipitationProbability: 45 },
+        ]
+    },
+    {
+        id: 5,
+        city: 'Oulu',
+        current: { temperatureC: 9.5, windKmh: 20, code: 3 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 7, temperatureMaxC: 10, windKmh: 20, code: 3, precipitationProbability: 60 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 6, temperatureMaxC: 9, windKmh: 18, code: 1, precipitationProbability: 50 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 19, code: 3, precipitationProbability: 55 },
+            { id: 4, day: 'Thursday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 20, code: 1, precipitationProbability: 45 },
+            { id: 5, day: 'Friday', temperatureMinC: 5, temperatureMaxC: 9, windKmh: 18, code: 3, precipitationProbability: 60 },
+            { id: 6, day: 'Saturday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 19, code: 2, precipitationProbability: 30 },
+            { id: 7, day: 'Sunday', temperatureMinC: 4, temperatureMaxC: 9, windKmh: 20, code: 1, precipitationProbability: 50 },
+        ]
+    },
+    {
+        id: 6,
+        city: 'Turku',
+        current: { temperatureC: 12.5, windKmh: 15, code: 2 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 15, code: 2, precipitationProbability: 25 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 16, code: 1, precipitationProbability: 35 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 15, code: 2, precipitationProbability: 20 },
+            { id: 4, day: 'Thursday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 15, code: 3, precipitationProbability: 40 },
+            { id: 5, day: 'Friday', temperatureMinC: 8, temperatureMaxC: 12, windKmh: 14, code: 1, precipitationProbability: 30 },
+            { id: 6, day: 'Saturday', temperatureMinC: 9, temperatureMaxC: 13, windKmh: 15, code: 2, precipitationProbability: 20 },
+            { id: 7, day: 'Sunday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 16, code: 1, precipitationProbability: 45 },
+        ]
+    },
+    {
+        id: 7,
+        city: 'Jyväskylä',
+        current: { temperatureC: 10.0, windKmh: 13, code: 1 },
+        forecast: [
+            { id: 1, day: 'Monday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 13, code: 1, precipitationProbability: 40 },
+            { id: 2, day: 'Tuesday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 14, code: 2, precipitationProbability: 25 },
+            { id: 3, day: 'Wednesday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 13, code: 3, precipitationProbability: 50 },
+            { id: 4, day: 'Thursday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 14, code: 1, precipitationProbability: 35 },
+            { id: 5, day: 'Friday', temperatureMinC: 6, temperatureMaxC: 10, windKmh: 13, code: 3, precipitationProbability: 45 },
+            { id: 6, day: 'Saturday', temperatureMinC: 7, temperatureMaxC: 11, windKmh: 14, code: 2, precipitationProbability: 20 },
+            { id: 7, day: 'Sunday', temperatureMinC: 5, temperatureMaxC: 9, windKmh: 13, code: 1, precipitationProbability: 50 },
+        ]
+    },
 ];
