af2ce0e0fad984f745f881b6bf3412bd2e273f3b
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-25 20:14:31 +0300

    add navigation to app

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import {StatusBar, StyleSheet, ScrollView} from 'react-native';
-import CurrentWeatherScreen from './screens/CurrentWeatherScreen';
-import {SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {LightTheme, DarkTheme} from "./theme/AppTheme";
 import {useState} from "react";
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from "@react-navigation/stack";
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import ForecastScreen from "./screens/ForecastScreen";
+import CurrentWeatherScreen from "./screens/CurrentWeatherScreen";
+import {MOCK_WEATHER, MOCK_WEATHER_WITH_FORECAST} from "./utils/mockWeather";
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -23,18 +24,24 @@ export default function App() {
     return (
         <PaperProvider theme={theme}>
 
-                    <NavigationContainer>
-                        <Stack.Navigator id='main-navigator'>
-                            <Stack.Screen
-                                name="Home"
-                                children={(props) => (
-                                    <CurrentWeatherScreen {...props} toggleTheme={toggleTheme}/>
-                                )}
-                                options={{headerShown: false}}
-                            />
-                        </Stack.Navigator>
-                    </NavigationContainer>
-                    <StatusBar barStyle='light-content' backgroundColor={theme.colors.background}/>
+            <NavigationContainer>
+                <Stack.Navigator id='main-navigator'>
+                    <Stack.Screen
+                        name="Home"
+                        children={(props) => (
+                            <CurrentWeatherScreen {...props} toggleTheme={toggleTheme}/>
+                        )}
+                        options={{headerShown: false}}
+                    />
+                    <Stack.Screen
+                        name="Forecast"
+                        component={ForecastScreen}
+                        initialParams={{weatherForecastData: MOCK_WEATHER_WITH_FORECAST[0]}}
+                        options={{headerShown: false}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+            <StatusBar barStyle='light-content' backgroundColor={theme.colors.background}/>
         </PaperProvider>
     );
 }
