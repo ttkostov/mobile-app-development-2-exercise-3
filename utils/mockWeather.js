7a61dc026a849a31f76dc0bf293a3754f67d23cd
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-25 16:25:13 +0300

    update mock weather for multiple cities

--- utils/mockWeather.js
+++ utils/mockWeather.js
@@ -1,6 +1,52 @@
-export const MOCK_WEATHER = {
-    city: 'Helsinki, FI',
-    temperatureC: 12.3,
-    windKmh: 18,
-    code: 1
-};
+// ChatGPT was used for generating the mock weather
+export const MOCK_WEATHER = [
+    {
+        id: 1,
+        city: 'Helsinki, FI',
+        temperatureC: 12.3,
+        windKmh: 18,
+        code: 1
+    },
+    {
+        id: 2,
+        city: 'Espoo, FI',
+        temperatureC: 12.0,
+        windKmh: 16,
+        code: 1
+    },
+    {
+        id: 3,
+        city: 'Tampere, FI',
+        temperatureC: 11.0,
+        windKmh: 14,
+        code: 3
+    },
+    {
+        id: 4,
+        city: 'Vantaa, FI',
+        temperatureC: 12.1,
+        windKmh: 17,
+        code: 2
+    },
+    {
+        id: 5,
+        city: 'Oulu, FI',
+        temperatureC: 9.5,
+        windKmh: 20,
+        code: 3
+    },
+    {
+        id: 6,
+        city: 'Turku, FI',
+        temperatureC: 12.5,
+        windKmh: 15,
+        code: 2
+    },
+    {
+        id: 7,
+        city: 'Jyväskylä, FI',
+        temperatureC: 10.0,
+        windKmh: 13,
+        code: 1
+    }
+];
